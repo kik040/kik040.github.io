ec6f0d1667b9be7a09365b5a7c6a4205a5c2a680
Sukanya Kaewbor <sukanya.kaewbor@gmail.com>
2022-02-18 14:16:34 +0700

    add age intro to js

--- intro-to-js/sukanya.js
+++ intro-to-js/sukanya.js
@@ -10,3 +10,21 @@ function askMyName() {
  const h1message = document.getElementById("name").innerHTML;
     alert(h1message + ' สถานะยากจน พร้อมพาทุกคนไปลำบาก')
  }
+
+ function calAge(){
+    const birthyear = prompt("คุณเกิดปี ค.ศ. ที่เท่าไหร่โปรดบอก");
+
+    if(birthyear != null){
+      let date=new Date();
+      const year = date.getFullYear();
+      const age = year - birthyear;
+      alert('ยินดีด้วยนะจ๊ะ คุณอยู่ในประเทศนี้มา '+ age +' ปีแล้ว 555 น้ำตาจะไหล');
+    }
+ }
+
+function loadProfile(){
+   const name = "Sukanya";
+   const province = "Nonthaburi";
+   document.getElementById("my-name").innerHTML = name;
+   document.getElementById("my-province").innerHTML = province;
+}
